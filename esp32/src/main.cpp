f283b8d6f24598d7a0c7c8cd6aaabe939bea244f
calebviliganilao-eng <your-email@example.com>
2026-09-20 16:06:29 +0800

    Configure initial Wokwi simulation and add FreeRTOS tasks

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,18 +1,13 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+
+extern void SensorTask(void *pvParameters);
+
 extern "C" void app_main(void) {
-    // PART II: Print recognizable startup message
     printf("BCA152 FreeRTOS Multisensor\n");
     printf("System starting...\n");
 
-    sensorQueue = xQueueCreate(1, sizeof(struct SensorData));
-    serialMutex = xSemaphoreCreateMutex();
-    systemEvents = xEventGroupCreate();
-
-    if (sensorQueue != NULL && serialMutex != NULL && systemEvents != NULL) {
-        xTaskCreate(DisplayTask, "Display", 4096, NULL, 1, NULL); 
-        xTaskCreate(SensorTask,  "Sensors", 4096, NULL, 2, NULL);  
-        xTaskCreate(AlarmTask,   "Alarm",   2048, NULL, 2, NULL);     
-        xTaskCreate(MotionTask,  "Motion",  2048, NULL, 3, NULL);   
-        xTaskCreate(InputTask,   "Input",   2048, NULL, 3, NULL);     
-        xTaskCreate(StateTask,   "State",   2048, NULL, 4, NULL);     
-    }
+    // Spawn the SensorTask at Priority 2
+    xTaskCreate(SensorTask, "Sensors", 4096, NULL, 2, NULL);
 }
