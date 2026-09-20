8b8c5804bce6947f9333994c4a276d180d103bcb
calebviliganilao-eng <your-email@example.com>
2026-09-21 01:06:04 +0800

    Refactored project into modular architecture and updated display task

---
+++ include/sensors.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void SensorTask(void *pvParameters);
+
+#ifdef __cplusplus
+}
+#endif
---
+++ include/system_state.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+enum class DisplayMode {
+    TEMPERATURE,
+    HUMIDITY,
+    LIGHT,
+    MOTION
+};
+
+extern DisplayMode currentDisplayMode;
